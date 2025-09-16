a2c5e3d0fea12a3ebe295db83e57e22c83a8e566
邹上豪 <apermesa3@gmail.com>
2025-09-16 14:32:56 +0800

    turtle

--- Test.py
+++ Test.py
@@ -1,31 +1,63 @@
-import turtle as t
-import math
-t.hideturtle()
-t.penup()
-t.goto(-50,50)
-t.pendown()
-t.right(90)
-t.forward(50)
-t.left(90)
-t.forward(100)
-t.right(90)
-t.forward(50)
-t.right(90)
-t.forward(50)
-t.right(90)
-t.forward(100)
-t.left(90)
-t.forward(50)
-t.left(135)
-t.forward(math.sqrt(2)*100)
-t.penup()
-t.goto(-50,0)
-t.pendown()
-t.forward(math.sqrt(2)*50)
-t.penup()
-t.goto(0,50)
-t.pendown()
-t.forward(math.sqrt(2)*50)
+# Hit the Target Game
+import turtle
 
+# Named constants
+SCREEN_WIDTH = 600       # Screen width
+SCREEN_HEIGHT = 600      # Screen height
+TARGET_LLEFT_X = 100     # Target's lower-left X
+TARGET_LLEFT_Y = 250     # Target's lower-left Y
+TARGET_WIDTH = 25        # Width of the target
+FORCE_FACTOR = 30        # Arbitrary force factor
+PROJECTILE_SPEED = 1     # Projectile's animation speed
+NORTH = 90               # Angle of north direction
+SOUTH = 270              # Angle of south direction
+EAST = 0                 # Angle of east direction
+WEST = 180               # Angle of west direction
 
-t.done()
+# Setup the window.
+turtle.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# Draw the target.
+turtle.hideturtle()
+turtle.speed(0)
+turtle.penup()
+turtle.goto(TARGET_LLEFT_X, TARGET_LLEFT_Y)
+turtle.pendown()
+turtle.setheading(EAST)
+turtle.forward(TARGET_WIDTH)
+turtle.setheading(NORTH)
+turtle.forward(TARGET_WIDTH)
+turtle.setheading(WEST)
+turtle.forward(TARGET_WIDTH)
+turtle.setheading(SOUTH)
+turtle.forward(TARGET_WIDTH)
+turtle.penup()
+
+# Center the turtle.
+turtle.goto(0, 0)
+turtle.setheading(EAST)
+turtle.showturtle()
+turtle.speed(PROJECTILE_SPEED)
+
+# Get the angle and force from the user.
+angle = float(input("Enter the projectile's angle: "))
+force = float(input("Enter the launch force (1-10): "))
+
+# Calculate the distance.
+distance = force * FORCE_FACTOR
+
+# Set the heading.
+turtle.setheading(angle)
+
+# Launch the projectile.
+turtle.pendown()
+turtle.forward(distance)
+
+# Did it hit the target?
+if (turtle.xcor() >= TARGET_LLEFT_X and
+        turtle.xcor() <= (TARGET_LLEFT_X + TARGET_WIDTH) and
+        turtle.ycor() >= TARGET_LLEFT_Y and
+        turtle.ycor() <= (TARGET_LLEFT_Y + TARGET_WIDTH)):
+    print('Target hit!')
+else:
+    print('You missed the target.')
